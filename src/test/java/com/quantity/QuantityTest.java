848816fd638a116709f1b26ff456a0d66b971808
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-18 21:41:34 +0530

    Added : TC 1.1 Ft 0 Ft 0 Equal

--- src/test/java/com/quantity/QuantityTest.java
+++ src/test/java/com/quantity/QuantityTest.java
@@ -19,4 +19,10 @@ public class QuantityTest {
 		Assert.assertEquals(feet1, feet2);
 	}
 
+	@Test
+	public void given0Feetand1Feet_ShouldReturnNotEqual() {
+		Feet feet1 = new Feet(0.0);
+		Feet feet2 = new Feet(1.0);
+		Assert.assertNotEquals(feet1, feet2);
+	}
 }
