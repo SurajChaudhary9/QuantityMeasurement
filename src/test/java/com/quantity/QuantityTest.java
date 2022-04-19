61bc9c473f8c8d7ee663015b41fcc4fa59e7f297
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-19 11:47:11 +0530

    Added : TC 1.8 Inch Null Check

--- src/test/java/com/quantity/QuantityTest.java
+++ src/test/java/com/quantity/QuantityTest.java
@@ -92,4 +92,12 @@ public class QuantityTest {
 		Inch inch2 = new Inch(1.0);
 		Assert.assertNotSame(inch1, inch2);
 	}
+
+	// TC 1.8 - Inch Null Check
+	@Test
+	public void given0InchAndNullInch_ShouldReturnNotEqual() {
+		Inch inch1 = new Inch(0.0);
+		Inch inch2 = null;
+		Assert.assertNotSame(inch1, inch2);
+	}
 }
