6491c80d5343a466ea01cd9053ab46a5ec1f8bb3
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-22 01:19:42 +0530

    Added : TC 1.6 : 1 kg = 1000 grams check

--- src/test/java/com/quantity/WeightQuantityTest.java
+++ src/test/java/com/quantity/WeightQuantityTest.java
@@ -19,12 +19,19 @@ public class WeightQuantityTest {
 	}
 
 	/**
-	 * Rigorous Test :- For Getting 0 Gram And 0 Gram Equality Check
+	 * TC :- For Getting 0 Gram + 0 Gram Equality Check
 	 */
 	@Test
 	public void given0Gramand0GramWhenEqual_ShouldReturnEqual() {
 		double value1 = quantityMeasurement.unitConversion(WeightUnits.GRAM, 0.0);
 		double value2 = quantityMeasurement.unitConversion(WeightUnits.GRAM, 0.0);
+		Assert.assertEquals(value1, value2, 0);
+	}
+
+	@Test
+	public void given1KGand1000GramsWhenEqual_ShouldReturnEqual() {
+		double value1 = quantityMeasurement.unitConversion(WeightUnits.KG, 1.0);
+		double value2 = quantityMeasurement.unitConversion(WeightUnits.GRAM, 1000.0);
 		Assert.assertEquals(value1, value2, 0);
 	}
 }
